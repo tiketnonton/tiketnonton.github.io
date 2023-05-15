f0ecaa2a65f076da4ff47406cc556fc7b9efd618
piw1 <roger.v.sitorus@gmail.com>
2023-05-15 22:47:36 +0800

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       colors: {
         background: "#004643",
+        background2: "#abd1c6",
         headline: "#fffffe",
+        headline2: "#001e1d",
+        subheadline: "#0f3433",
         paragraph: "#abd1c6",
         button: "#f9bc60",
         buttontext: "#001e1d",
